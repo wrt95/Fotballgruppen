acdc72e5c08a8a58199f746cc7a0295d9ad42134
wrt95 <wrt_95@hotmail.com>
2024-11-12 14:23:06 +0100

    fixing deploy

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,16 +1,21 @@
-import { Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import { LandingPage } from "../pages/LandingPage";
 import { WomensWLPage } from "../pages/WomensWLPage";
 import { MensWLPage } from "../pages/MensWLPage";
 import { RoutePaths } from "./routePaths";
 
 export const AppRoutes = () => (
-  <Routes>
-    <Route path={RoutePaths.ROOT} element={<LandingPage />} />
-    <Route
-      path={RoutePaths.WOMENS_TEAM_WINTER_LEAGUE}
-      element={<WomensWLPage />}
-    />
-    <Route path={RoutePaths.MENS_TEAM_WINTER_LEAGUE} element={<MensWLPage />} />
-  </Routes>
+  <Router>
+    <Routes>
+      <Route path={RoutePaths.ROOT} element={<LandingPage />} />
+      <Route
+        path={RoutePaths.WOMENS_TEAM_WINTER_LEAGUE}
+        element={<WomensWLPage />}
+      />
+      <Route
+        path={RoutePaths.MENS_TEAM_WINTER_LEAGUE}
+        element={<MensWLPage />}
+      />
+    </Routes>
+  </Router>
 );
